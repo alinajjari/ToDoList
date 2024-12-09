ba43f0d14b3d48f0e47867cf5a8dcc29967f9a85
alinajjari <dev.alinajjari@gmail.com>
2024-12-09 11:43:40 +0330

    create themeplate ToDoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,208 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid grid-cols-12 gap-x-6">
+      <div className="col-span-4 bg-slate-800 bg-opacity-40 rounded-xl p-6 max-h-[90vh] overflow-y-auto relative">
+        <div className="bg-slate-700 p-2 text-lg font-bold text-white rounded-full text-center mb-6">
+          New Task...
+        </div>
+        <form className="text-white">
+          <input className="w-full bg-slate-900 rounded-full p-3 mb-3 placeholder:text-xs focus:outline-none" type="text" placeholder="Add Title..." name="" id="" />
+          <select className="w-full bg-slate-900 rounded-full p-3 mb-3 placeholder:text-xs focus:outline-none" placeholder="Status..." name="" id="">
+            <option value="s">s</option>
+          </select>
+          <textarea className="w-full bg-slate-900 rounded-xl p-3 mb-3 placeholder:text-xs focus:outline-none h-28" placeholder="Add Description..." name="" id=""></textarea>
+          <button className="bg-orange-700 w-full p-3 text-center rounded-full cursor-pointer" type="submit">Add Task...</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="col-span-8 bg-slate-800 bg-opacity-40 rounded-xl p-6 max-h-[90vh] overflow-y-auto">
+        <div className="border-b border-slate-800 p-2 leading-6 relative mb-4 overflow-hidden">
+          <div className="flex items-center justify-between">
+          <div className="basis-4/5">
+          <h3 className="text-white mb-2 text-base">Lorem ipsum dolor sit amet.</h3>
+          <div className="text-xs flex items-center mb-2">
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
+          </svg>
+          12:45
+            </span>
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+          </svg>
+          Status: Done
+          </span>
+          </div>
+          </div>
+          <div className="basis-1/5 flex justify-end items-center gap-x-3">
+            <button className="text-stone-400">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
+            </svg>
+            </button>
+            <input type="checkbox" className=" h-5 w-5 cursor-pointer transition-all accent-slate-900 rounded shadow hover:shadow-md border border-slate-700 checked:bg-stone-400 checked:border-stone-400" name="" id="" />
+          </div>
+          </div>
+          <div className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minima sit totam cupiditate debitis ullam?
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-36 text-slate-950 opacity-30 -z-10 -rotate-45 absolute -bottom-10 -right-10">
+          <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+        </svg>
+
+        </div>
+        <div className="border-b border-slate-800 p-2 leading-6 relative mb-4 overflow-hidden">
+          <div className="flex items-center justify-between">
+          <div className="basis-4/5">
+          <h3 className="text-white mb-2 text-base">Lorem ipsum dolor sit amet.</h3>
+          <div className="text-xs flex items-center mb-2">
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
+          </svg>
+          12:45
+            </span>
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+          </svg>
+          Status: Done
+          </span>
+          </div>
+          </div>
+          <div className="basis-1/5 flex justify-end items-center gap-x-3">
+            <button className="text-stone-400">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
+            </svg>
+            </button>
+            <input type="checkbox" className=" h-5 w-5 cursor-pointer transition-all accent-slate-900 rounded shadow hover:shadow-md border border-slate-700 checked:bg-stone-400 checked:border-stone-400" name="" id="" />
+
+          </div>
+          </div>
+          <div className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minima sit totam cupiditate debitis ullam?
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-36 text-slate-950 opacity-30 -z-10 -rotate-45 absolute -bottom-10 -right-10">
+          <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+        </svg>
+
+        </div>
+        <div className="border-b border-slate-800 p-2 leading-6 relative mb-4 overflow-hidden">
+          <div className="flex items-center justify-between">
+          <div className="basis-4/5">
+          <h3 className="text-white mb-2 text-base">Lorem ipsum dolor sit amet.</h3>
+          <div className="text-xs flex items-center mb-2">
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
+          </svg>
+          12:45
+            </span>
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+          </svg>
+          Status: Done
+          </span>
+          </div>
+          </div>
+          <div className="basis-1/5 flex justify-end items-center gap-x-3">
+            <button className="text-stone-400">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
+            </svg>
+            </button>
+            <input type="checkbox" className=" h-5 w-5 cursor-pointer transition-all accent-slate-900 rounded shadow hover:shadow-md border border-slate-700 checked:bg-stone-400 checked:border-stone-400" name="" id="" />
+
+          </div>
+          </div>
+          <div className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minima sit totam cupiditate debitis ullam?
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-36 text-slate-950 opacity-30 -z-10 -rotate-45 absolute -bottom-10 -right-10">
+          <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+        </svg>
+
+        </div>
+        <div className="border-b border-slate-800 p-2 leading-6 relative mb-4 overflow-hidden">
+          <div className="flex items-center justify-between">
+          <div className="basis-4/5">
+          <h3 className="text-white mb-2 text-base">Lorem ipsum dolor sit amet.</h3>
+          <div className="text-xs flex items-center mb-2">
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
+          </svg>
+          12:45
+            </span>
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+          </svg>
+          Status: Done
+          </span>
+          </div>
+          </div>
+          <div className="basis-1/5 flex justify-end items-center gap-x-3">
+            <button className="text-stone-400">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
+            </svg>
+            </button>
+            <input type="checkbox" className=" h-5 w-5 cursor-pointer transition-all accent-slate-900 rounded shadow hover:shadow-md border border-slate-700 checked:bg-stone-400 checked:border-stone-400" name="" id="" />
+
+          </div>
+          </div>
+          <div className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minima sit totam cupiditate debitis ullam?
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-36 text-slate-950 opacity-30 -z-10 -rotate-45 absolute -bottom-10 -right-10">
+          <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+        </svg>
+
+        </div>
+        <div className="border-b border-slate-800 p-2 leading-6 relative mb-4 overflow-hidden">
+          <div className="flex items-center justify-between">
+          <div className="basis-4/5">
+          <h3 className="text-white mb-2 text-base">Lorem ipsum dolor sit amet.</h3>
+          <div className="text-xs flex items-center mb-2">
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
+          </svg>
+          12:45
+            </span>
+            <span className="flex text-stone-600">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+          </svg>
+          Status: Done
+          </span>
+          </div>
+          </div>
+          <div className="basis-1/5 flex justify-end items-center gap-x-3">
+            <button className="text-stone-400">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
+            </svg>
+            </button>
+            <input type="checkbox" className=" h-5 w-5 cursor-pointer transition-all accent-slate-900 rounded shadow hover:shadow-md border border-slate-700 checked:bg-stone-400 checked:border-stone-400" name="" id="" />
+
+          </div>
+          </div>
+          <div className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minima sit totam cupiditate debitis ullam?
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-36 text-slate-950 opacity-30 -z-10 -rotate-45 absolute -bottom-10 -right-10">
+          <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+        </svg>
+
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
