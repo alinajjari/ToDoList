ffa6f7020b6d47b40cd8f911169fa305e4f1e893
alinajjari <dev.alinajjari@gmail.com>
2024-12-10 09:01:17 +0330

    style

--- src/Components/NewTask.jsx
+++ src/Components/NewTask.jsx
@@ -29,7 +29,9 @@ function NewTask() {
     <form className="text-white" onSubmit={handleSubmit}>
           <input name="title" value={formInput.title} onChange={handleChangeInput} className="w-full bg-slate-900 rounded-full p-3 mb-3 placeholder:text-xs focus:outline-none" type="text" placeholder="Add Title..." id="" />
           <select className="w-full bg-slate-900 rounded-full p-3 mb-3 placeholder:text-xs focus:outline-none" placeholder="Status..." name="" id="">
-            <option value="s">s</option>
+            <option value="digikala">s</option>
+            <option value="apple">s</option>
+            <option value="snapp">s</option>
           </select>
           <textarea name="desc" value={formInput.desc} onChange={handleChangeInput} className="w-full bg-slate-900 rounded-xl p-3 mb-3 placeholder:text-xs focus:outline-none h-28" placeholder="Add Description..." id=""></textarea>
           <button onClick={handleSubmit} className="bg-orange-700 w-full p-3 text-center rounded-full cursor-pointer" type="submit">Add Task...</button>
